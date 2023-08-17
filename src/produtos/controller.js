109049c8a78bdb8120e3a9730868cf7f7ba3488d
Rodrigo César Perissini <rodrigo.cesar.perissini@gmail.com>
2023-08-17 15:11:58 -0300

    fix: changed return message for update

--- src/produtos/controller.js
+++ src/produtos/controller.js
@@ -33,7 +33,7 @@ router.patch('/:id', async (req,res) => {
     const id = req.params.id
     const body = req.body
     const produtos = await queries.update(id, body)
-    res.status(200).json(produtos)
+    res.status(200).json('Produto atualizado com sucesso!')
 })
 
 module.exports = router
